2cae3d6391193254902d0f67fd9075ae53de34f8
sukanta255 <sukantap872@gmail.com>
2023-05-21 14:09:54 +0530

    assignment final submit

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,16 +5,25 @@ const userRouter=express.Router()
 
 //Handel GET Request to "/api/users" and getting the data from database.json file
 userRouter.get("/users",(req,res)=>{
-    const data = JSON.parse(fs.readFileSync("./database.json","utf-8"))
-    res.send(data.users);
+    try{
+        const data = JSON.parse(fs.readFileSync("./database.json","utf-8"))
+        res.send(data.users);
+    }catch(err){
+        console.log(err);
+    }
 })
 
 //Handel POST Request to "/api/users" and save user information to database.json file
 userRouter.post("/users",(req,res)=>{
-    const data = JSON.parse(fs.readFileSync("./database.json","utf-8"))
-    data.users.push(req.body);
-    fs.writeFileSync("./database.json",JSON.stringify(data))
-    res.status(201).json({message: "User information added succesfully" })
+    try{
+        const data = JSON.parse(fs.readFileSync("./database.json","utf-8"))
+        data.users.push(req.body);
+        fs.writeFileSync("./database.json",JSON.stringify(data))
+        res.status(201).json({message: "User information added succesfully" })
+    }catch(err){
+        console.log(err)
+        res.status(500).send({message: "Internal Server Error", err});
+    }
 })
 
 // export userRouter
